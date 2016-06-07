3ed10c7eca6b95cf3aa325f3be3dada2c21fff7e
snagannagari <snagannagari@csc.com>
2016-06-07 19:22:31 +0530

    F3 commit

--- WebApplication1/WebApplication1/Controllers/FirstController.cs
+++ WebApplication1/WebApplication1/Controllers/FirstController.cs
@@ -17,5 +17,9 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public ActionResult ThirdView()
+        {
+            return View();
+        }
     }
 }
